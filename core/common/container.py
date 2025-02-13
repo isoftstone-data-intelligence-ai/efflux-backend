bf13d91d4a11ea1365b787916605718fdb580568
Qipengpai <ppqif@isoftstone.com>
2025-02-13 11:23:27 +0800

    模型选择逻辑，llm初始化

--- core/common/container.py
+++ core/common/container.py
@@ -1,5 +1,7 @@
 from dependency_injector import containers, providers
 
+from dao.llm_config_dao import LlmConfigDAO
+from dao.llm_template_dao import LlmTemplateDAO
 from extensions.ext_database import DatabaseProvider
 from dao.user_dao import UserDAO
 from dao.chat_window_dao import ChatWindowDAO
@@ -33,10 +35,22 @@ class Container(containers.DeclarativeContainer):
     # 注册chat_window Service
     chat_window_service = providers.Singleton(ChatWindowService, chat_window_dao=chat_window_dao)
 
+    # 注册llm_config DAO
+    llm_config_dao = providers.Singleton(LlmConfigDAO, session_factory=database_provider.provided.session_factory)
+    # 注册llm_template DAO
+    llm_template_dao = providers.Singleton(LlmTemplateDAO, session_factory=database_provider.provided.session_factory)
+
+    # 注册LLM Manager
+    llm_manager = providers.Singleton(LLMManager, )
+
+
     # 注册模型
     llm = providers.Singleton(AzureLlm)
 
     # 注册 chat Service
-    chat_service = providers.Singleton(ChatService, llm=llm, mcp_config_service=mcp_config_service, chat_window_dao=chat_window_dao)
+    chat_service = providers.Singleton(ChatService,
+                                       mcp_config_service=mcp_config_service,
+                                       chat_window_dao=chat_window_dao,
+                                       llm_config_dao=LlmConfigDAO)
 
 
